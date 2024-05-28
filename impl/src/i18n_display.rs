cc1ca8e0de240974d2eca51754facfe60403d1ff
daisuke ito <daisuke.ito.cs@gmail.com>
2024-05-28 10:23:10 +0900

    Feature/proto type (#1)
    * feat: Impl ErrorTranslator
    
    * chore: Add i18n metadata
    
    * chore: Add comments for new crate
    
    * feat: Proto type of I18nError
    
    * feat: Completed to create proto type
    
    * refactor: Separate Macro and trait/struct
    
    * refactor: Add Fr and remove Ja
    
    * chore: Add empty README

---
+++ impl/src/i18n_display.rs
@@ -0,0 +1,8 @@
+// pub trait ToI18nString {
+//     fn to_i18n_string(&self, language_code: LanguageCode) -> String;
+// }
+
+// pub enum LanguageCode {
+//     En,
+//     Ja,
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
+pub mod language_code;
+pub mod to_i18n_string;
 
+pub use i18n_error_impl::*;
+pub use language_code::*;
+pub use to_i18n_string::*;
